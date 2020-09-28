d7afa9c8a044b677fea1410a9f3b3ae18db17f52
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-28 13:03:21 +0530

    MEAN

--- AngularApp/src/app/shared/employee.service.ts
+++ AngularApp/src/app/shared/employee.service.ts
@@ -1,18 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/toPromise";
 
-import { Employee } from './employee.model';
+import { Employee } from "./employee.model";
 
 @Injectable()
 export class EmployeeService {
   selectedEmployee: Employee;
   employees: Employee[];
-  readonly baseURL = 'http://localhost:3000/employees';
+  readonly baseURL = "http://localhost:3000/employees";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   postEmployee(emp: Employee) {
     return this.http.post(this.baseURL, emp);
@@ -29,5 +28,4 @@ export class EmployeeService {
   deleteEmployee(_id: string) {
     return this.http.delete(this.baseURL + `/${_id}`);
   }
-
 }
